2d9b217d9fe40c0105c04d2d42ad68aae39a3686
Tianyi Xie <1213703937@qq.com>
2021-11-05 04:34:13 +0000

    Debug

--- percolate.c
+++ percolate.c
@@ -204,7 +204,7 @@ int main(int argc, char *argv[])
     for(i = 0; i < MPROC; ++i) {
       for(j = 0; j < NPROC; ++j) {
         for(k = 0; k < M; ++k) {
-          MPI_Send(&map[k+i*MPROC][j*NPROC], N, MPI_INT, i*MPROC+j, 0, comm);
+          MPI_Ssend(&map[k+i*MPROC][j*NPROC], N, MPI_INT, i*MPROC+j, 0, comm);
         }
       }
     }
@@ -385,7 +385,7 @@ int main(int argc, char *argv[])
   }
   else {
     for (int i = 0; i < M; ++i) {
-      MPI_Send(&smallmap[i][0], N, MPI_INT, 0, 0, comm);
+      MPI_Ssend(&smallmap[i][0], N, MPI_INT, 0, 0, comm);
     }
   }
 
